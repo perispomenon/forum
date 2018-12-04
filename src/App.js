fe88a9ac608c04eb997aed69842293f9ee6619ba
Anatoly.Golubev <Anatoly.Golubev@DESKTOP-U46H1F9>
2018-12-04 14:57:58 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Navigation from './components/Navigation'
+import About from './pages/About'
+import Home from './pages/Home'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
         <Navigation></Navigation>
-        <div className="container">
-          <div className="jumbotron">
-            <h1 className="display-4">Hello, world!</h1>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr className="my-4"/>
-            <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-          </div>
-        </div>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/about' component={About}></Route>
+        </Switch>
       </div>
     )
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class Navigation extends React.Component {
   render () {
     return (
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <Link to='/' className='navbar-brand'>Forum</Link>
+        <NavLink to='/' className='navbar-brand'>Forum</NavLink>
         <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon' />
         </button>
@@ -14,22 +14,22 @@ class Navigation extends React.Component {
           {/* LEFT NAV */}
           <ul className='navbar-nav'>
             <li className='nav-item active'>
-              <Link to='/' className='nav-link'>Home</Link>
+              <NavLink to='/' className='nav-link'>Home</NavLink>
             </li>
             <li className='nav-item'>
-              <Link to='/themes' className='nav-link'>Browse Themes</Link>
+              <NavLink to='/themes' className='nav-link'>Browse Themes</NavLink>
             </li>
             <li className='nav-item'>
-              <Link to='/about' className='nav-link'>About</Link>
+              <NavLink to='/about' className='nav-link'>About</NavLink>
             </li>
           </ul>
           {/* RIGHT NAV */}
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link to='/signup' className='nav-link'>Sign up</Link>
+              <NavLink to='/signup' className='nav-link'>Sign up</NavLink>
             </li>
             <li className="nav-item">
-              <Link to='/login' className='nav-link'>Login</Link>
+              <NavLink to='/login' className='nav-link'>Login</NavLink>
             </li>
           </ul>
         </div>
